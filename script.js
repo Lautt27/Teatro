d184ea6ace9ab9c8ce7de3f0c6eab12d0aecdadb
Lautaro Casal <Laucasal27@gmail.com>
2024-07-29 15:09:42 -0300

    Updated

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         const target = document.querySelector(this.getAttribute('href'));
-        const offset = 0;
+        const offset = 0; 
 
         window.scrollTo({
             top: target.offsetTop - offset,
             behavior: 'smooth'
         });
     });
+});
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
 });
